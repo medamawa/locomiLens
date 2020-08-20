4dd136e6a83122f34dba0e4efb3dfcaadeda49e3
SamidareGeorge <sogo116@icloud.com>
2020-08-21 00:55:40 +0900

    locomiへの画面遷移を改善

--- ARCoreLocationLesson/LocomiViewController.swift
+++ ARCoreLocationLesson/LocomiViewController.swift
@@ -7,23 +7,61 @@
 //
 
 import UIKit
+import SwiftUI
 
 class LocomiViewController: UIViewController {
     
     override func viewDidLoad() {
-        view.backgroundColor = .systemIndigo
+        super.viewDidLoad()
         
-        let url = URL(string: "locomi://")!
-        if UIApplication.shared.canOpenURL(url) {
-            if #available(iOS 10.0, *) {
-                UIApplication.shared.open(url, options: [:], completionHandler: {
-                    (success) in
-                    print("Open \(success)")
-                })
-            }else{
-                UIApplication.shared.openURL(url)
+        let vc: UIHostingController = UIHostingController(rootView: LocomiView())
+        self.addChild(vc)
+        self.view.addSubview(vc.view)
+        vc.didMove(toParent: self)
+        
+        vc.view.translatesAutoresizingMaskIntoConstraints = false
+        vc.view.heightAnchor.constraint(equalToConstant: 320).isActive = true
+        vc.view.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 16).isActive = true
+        vc.view.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -16).isActive = true
+        vc.view.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+    }
+    
+}
+
+
+struct LocomiView: View {
+    
+    var body: some View {
+        
+        Button(action: {
+            let url = URL(string: "locomi://")!
+            if UIApplication.shared.canOpenURL(url) {
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(url, options: [:], completionHandler: {
+                        (success) in
+                        print("Open \(success)")
+                    })
+                }else{
+                    UIApplication.shared.openURL(url)
+                }
             }
+        }) {
+            HStack {
+                Image(systemName: "mappin.and.ellipse")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.white)
+                Text("locomi")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.white)
+                Image(systemName: "arrowshape.turn.up.right")
+                    .font(.largeTitle)
+                    .foregroundColor(Color.white)
+            }
+            .padding(.all)
+            .background(Color.blue)
+            .cornerRadius(10)
         }
+        
     }
     
 }
