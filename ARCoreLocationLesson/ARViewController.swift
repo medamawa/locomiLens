f03fc5a217012b86d1264d6bf29361a900d1b790
SamidareGeorge <sogo116@icloud.com>
2020-08-21 21:07:56 +0900

    標高変化に対応できるようにした

--- ARCoreLocationLesson/ARViewController.swift
+++ ARCoreLocationLesson/ARViewController.swift
@@ -10,16 +10,23 @@ import UIKit
 import ARCL
 import CoreLocation
 
-class ARViewController: UIViewController {
+class ARViewController: UIViewController, CLLocationManagerDelegate {
     
     var sceneLocationView = SceneLocationView()
+    var locationManager: CLLocationManager!
     var spotsData: [(CLLocation, UIImage)] = []
     var comics: [Comic]?
+    var location: CLLocation? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupLocationManager()
         
-        comics = APIRequest().getNearComics(latitude: "34.821413", longitude: "135.538147")
+        let coordinate = self.locationManager.location != nil ? self.locationManager.location!.coordinate: CLLocationCoordinate2D()
+        print(String(coordinate.latitude))
+        print(String(coordinate.longitude))
+        
+        comics = APIRequest().getNearComics(latitude: String(coordinate.latitude), longitude: String(coordinate.longitude))
         for comic in comics! {
             let coordinate = CLLocationCoordinate2D(latitude: comic.lat, longitude: comic.lng)
             let location = CLLocation(coordinate: coordinate, altitude: 45)
@@ -47,12 +54,36 @@ class ARViewController: UIViewController {
       sceneLocationView.frame = view.bounds
     }
     
+    // 位置情報取得 start
+    func setupLocationManager() {
+        locationManager = CLLocationManager()
+        guard let locationManager = locationManager else { return }
+        locationManager.requestWhenInUseAuthorization()
+
+        let status = CLLocationManager.authorizationStatus()
+        if status == .authorizedWhenInUse {
+            locationManager.delegate = self
+            locationManager.distanceFilter = 10
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first
+        self.location = location
+    }
+    // 位置情報取得 end
+    
     @objc func onTap(sender: UIButton) {
+        // データ初期化
+        spotsData = []
+        
         comics = APIRequest().getNearComics(latitude: "34.821413", longitude: "135.538147")
         
         for comic in comics! {
             let coordinate = CLLocationCoordinate2D(latitude: comic.lat, longitude: comic.lng)
-            let location = CLLocation(coordinate: coordinate, altitude: 30)
+            print(self.location?.altitude)
+            let location = CLLocation(coordinate: coordinate, altitude: self.location?.altitude ?? 10)
             let image = Utility().drawText(text: comic.text)
             spotsData.append((location, image!))
         }
