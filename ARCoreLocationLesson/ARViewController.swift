94633cb8f33c8f214359c2880d421ff5e82a5f2b
SamidareGeorge <sogo116@icloud.com>
2020-08-21 19:47:02 +0900

    ARのImageラベルの改善、投稿のAR表示

--- ARCoreLocationLesson/ARViewController.swift
+++ ARCoreLocationLesson/ARViewController.swift
@@ -14,29 +14,59 @@ class ARViewController: UIViewController {
     
     var sceneLocationView = SceneLocationView()
     var spotsData: [(CLLocation, UIImage)] = []
+    var comics: [Comic]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-                
-        let coordinate1 = CLLocationCoordinate2D(latitude: 34.812323475848686, longitude: 135.53659200668335)
-        let location1 = CLLocation(coordinate: coordinate1, altitude: 30)
-        let image1 = Utility().drawText(image: UIImage(named: "pocky")!, text: "万博公園")
-        let coordinate2 = CLLocationCoordinate2D(latitude: 34.822259043034734, longitude: 135.5382227897644)
-        let location2 = CLLocation(coordinate: coordinate2, altitude: 30)
-        let image2 = Utility().drawText(image: UIImage(named: "pocky")!, text: "ゴルフ場")
-        spotsData = [(location1, image1!), (location2, image2!)]
+        
+//        let coordinate1 = CLLocationCoordinate2D(latitude: 34.812323475848686, longitude: 135.53659200668335)
+//        let location1 = CLLocation(coordinate: coordinate1, altitude: 30)
+//        let image1 = Utility().drawText(text: "万博公園ー太陽の塔だよ。文字数稼ぎよわあ")
+//
+//        let coordinate2 = CLLocationCoordinate2D(latitude: 34.822259043034734, longitude: 135.5382227897644)
+//        let location2 = CLLocation(coordinate: coordinate2, altitude: 30)
+//        let image2 = Utility().drawText(text: "ゴルフ場")
+//        spotsData = [(location1, image1!), (location2, image2!)]
+        
+        comics = APIRequest().getNearComics(latitude: "34.821413", longitude: "135.538147")
+        for comic in comics! {
+            let coordinate = CLLocationCoordinate2D(latitude: comic.lat, longitude: comic.lng)
+            let location = CLLocation(coordinate: coordinate, altitude: 45)
+            let image = Utility().drawText(text: comic.text)
+            spotsData.append((location, image!))
+        }
+        print(spotsData)
         
         Utility().addLocations(sceneLocationView: sceneLocationView, spotsData: spotsData)
         
         sceneLocationView.run()
-
         view.addSubview(sceneLocationView)
+        
+        let button = UIButton(type: .system)
+        button.setTitle("更新する", for: .normal)
+        button.sizeToFit()
+        button.center = view.center
+        button.addTarget(self, action: #selector(onTap), for: .touchUpInside)
+        view.addSubview(button)
     }
-
+    
     override func viewDidLayoutSubviews() {
       super.viewDidLayoutSubviews()
 
       sceneLocationView.frame = view.bounds
+    }
+    
+    @objc func onTap(sender: UIButton) {
+        comics = APIRequest().getNearComics(latitude: "34.821413", longitude: "135.538147")
+        
+        for comic in comics! {
+            let coordinate = CLLocationCoordinate2D(latitude: comic.lat, longitude: comic.lng)
+            let location = CLLocation(coordinate: coordinate, altitude: 30)
+            let image = Utility().drawText(text: comic.text)
+            spotsData.append((location, image!))
+        }
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "dataLoad"), object: nil)
+        print(spotsData)
     }
 
 }
--- Utilities/Utility.swift
+++ Utilities/Utility.swift
@@ -11,6 +11,31 @@ import ARKit
 import CoreLocation
 
 class Utility {
+
+    /// 条件をクリアするまで待つ
+    ///
+    /// - Parameters:
+    ///   - waitContinuation: 待機条件
+    ///   - compleation: 通過後の処理
+    func wait(_ waitContinuation: @escaping (()->Bool), compleation: @escaping (()->Void)) {
+        var wait = waitContinuation()
+        // 0.01秒周期で待機条件をクリアするまで待つ
+        let semaphore = DispatchSemaphore(value: 0)
+        DispatchQueue.global().async {
+            while wait {
+                DispatchQueue.main.async {
+                    wait = waitContinuation()
+                    semaphore.signal()
+                }
+                semaphore.wait()
+                Thread.sleep(forTimeInterval: 0.01)
+            }
+            // 待機条件をクリアしたので通過後の処理を行う
+            DispatchQueue.main.async {
+                compleation()
+            }
+        }
+    }
     
     /// 複数のスポットを追加する
     ///
@@ -31,8 +56,26 @@ class Utility {
     /// - Parameters:
     ///   - image: 追加先の元画像
     ///   - text: 追加するテキスト   
-    func drawText(image: UIImage, text: String) -> UIImage?
+    func drawText(text: String) -> UIImage?
     {
+        var newImage: UIImage?
+        
+        if text.count <= 9 {
+            newImage = smallImage(text: text)
+        } else if text.count <= 17 {
+            newImage = bigImage(text: text)
+        } else {
+            let t = String(text.prefix(17)) + "..."
+            newImage = bigImage(text: t)
+        }
+
+        return newImage
+    }
+    
+    private func smallImage(text: String) -> UIImage?
+    {
+        let image = UIImage(named: "hukidashi")!
+        
         let font = UIFont.boldSystemFont(ofSize: 32)
         let imageRect = CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height)
 
@@ -40,7 +83,7 @@ class Utility {
 
         image.draw(in: imageRect)
 
-        let textRect  = CGRect(x: 5, y: 5, width: image.size.width - 5, height: image.size.height - 5)
+        let textRect  = CGRect(x: 10, y: 10, width: image.size.width - 5, height: image.size.height - 5)
         let textStyle = NSMutableParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
         let textFontAttributes = [
             NSAttributedString.Key.font: font,
@@ -48,6 +91,37 @@ class Utility {
             NSAttributedString.Key.paragraphStyle: textStyle
         ]
         text.draw(in: textRect, withAttributes: textFontAttributes)
+
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+
+        UIGraphicsEndImageContext()
+
+        return newImage
+    }
+    
+    private func bigImage(text: String) -> UIImage?
+    {
+        let image = UIImage(named: "hukidashi-big")!
+        let t1 = String(text.prefix(9))
+        let t2 = String(text.suffix(text.count - 9))
+        
+        let font = UIFont.boldSystemFont(ofSize: 32)
+        let imageRect = CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height)
+
+        UIGraphicsBeginImageContext(image.size);
+
+        image.draw(in: imageRect)
+
+        let textRect1 = CGRect(x: 10, y: 10, width: image.size.width - 5, height: image.size.height - 5)
+        let textRect2 = CGRect(x: 10, y: 48, width: image.size.width - 5, height: image.size.height - 5)
+        let textStyle = NSMutableParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
+        let textFontAttributes = [
+            NSAttributedString.Key.font: font,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.paragraphStyle: textStyle
+        ]
+        t1.draw(in: textRect1, withAttributes: textFontAttributes)
+        t2.draw(in: textRect2, withAttributes: textFontAttributes)
 
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
 
