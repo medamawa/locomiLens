e287409f3777d14ef066afafd850476bca9da737
SamidareGeorge <sogo116@icloud.com>
2020-08-21 19:54:34 +0900

    ARのサンプルスポットデータを消去

--- ARCoreLocationLesson/ARViewController.swift
+++ ARCoreLocationLesson/ARViewController.swift
@@ -19,15 +19,6 @@ class ARViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let coordinate1 = CLLocationCoordinate2D(latitude: 34.812323475848686, longitude: 135.53659200668335)
-//        let location1 = CLLocation(coordinate: coordinate1, altitude: 30)
-//        let image1 = Utility().drawText(text: "万博公園ー太陽の塔だよ。文字数稼ぎよわあ")
-//
-//        let coordinate2 = CLLocationCoordinate2D(latitude: 34.822259043034734, longitude: 135.5382227897644)
-//        let location2 = CLLocation(coordinate: coordinate2, altitude: 30)
-//        let image2 = Utility().drawText(text: "ゴルフ場")
-//        spotsData = [(location1, image1!), (location2, image2!)]
-        
         comics = APIRequest().getNearComics(latitude: "34.821413", longitude: "135.538147")
         for comic in comics! {
             let coordinate = CLLocationCoordinate2D(latitude: comic.lat, longitude: comic.lng)
